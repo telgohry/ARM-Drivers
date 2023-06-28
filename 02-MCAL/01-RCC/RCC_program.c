b177185ecbc5d8a328d5221aedc76bf92988448d
TarekGohry <telgohry@gmail.com>
2023-06-28 22:44:06 +0300

    Implemented RCC Driver

---
+++ 02-MCAL/01-RCC/RCC_program.c
@@ -0,0 +1,72 @@
+/*************************************************************************************************/
+/* Author    : Tarek Gohry                                                                       */
+/* Version   : V01                                                                               */
+/* Date      : 25 June 2023                                                                      */
+/*************************************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
+
+void RCC_voidInitSysClock(void)
+{
+	#if    RCC_CLOCK_TYPE == RCC_HSE_CRYSTAL
+		RCC_CR   = 0x00010000; /* Enable HSE with no pypass */
+		RCC_CFGR = 0x00000001;
+	#elif  RCC_CLOCK_TYPE == RCC_HSE_RC
+		RCC_CR   = 0x00050000; /* Enable HSE with pypass */
+		RCC_CFGR = 0x00000001;
+	#elif  RCC_CLOCK_TYPE == RCC_HSI
+		RCC_CR   = 0x00000081; /* Enable HSI */
+		RCC_CFGR = 0x00000000;
+	#elif  RCC_CLOCK_TYPE == RCC_PLL
+		#if    RCC_PLL_INPUT == RCC_PLL_IN_HSE
+			CLR_BIT(RCC_CFGR,PLL_XTPRE);
+		#elif  RCC_PLL_INPUT == RCC_PLL_IN_HSE_DIV_2
+			SET_BIT(RCC_CFGR,PLL_XTPRE);
+		#elif  RCC_PLL_INPUT == RCC_PLL_IN_HSI_DIV_2
+			CLR_BIT(RCC_CFGR,PLL_SRC);
+		#else  
+			#error("You choosed wrong clock type")
+		#endif
+	#else
+		#error("You choosed wrong clock type")
+	#endif
+}
+
+void RCC_voidEnableClock(u8 Copy_u8BusId, u8 Copy_u8PerId)
+{
+	if(Copy_u8PerId <= 31)
+	{
+		switch(Copy_u8BusId)
+		{
+			case RCC_AHB    : SET_BIT(RCC_AHBENR  , Copy_u8PerId);   break;
+			case RCC_APB1   : SET_BIT(RCC_APB1ENR , Copy_u8PerId);   break;
+			case RCC_APB2   : SET_BIT(RCC_APB2ENR , Copy_u8PerId);   break;	
+		}
+	}
+	else
+	{
+		/* Return Error */
+	}
+	
+}
+
+void RCC_voidDisableClock(u8 Copy_u8BusId, u8 Copy_u8PerId)
+{
+	if(Copy_u8PerId <= 31)
+	{
+		switch(Copy_u8BusId)
+		{
+			case RCC_AHB    : CLR_BIT(RCC_AHBENR  , Copy_u8PerId);   break;
+			case RCC_APB1   : CLR_BIT(RCC_APB1ENR , Copy_u8PerId);   break;
+			case RCC_APB2   : CLR_BIT(RCC_APB2ENR , Copy_u8PerId);   break;	
+		}
+	}
+	else
+	{
+		/* Return Error */
+	}	
+}
